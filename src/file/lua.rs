64c9ff92f68082e4e24ecf078ddf495aed0b4cfa
manshanko <manshanko@wartide.net>
2025-09-23 07:04:34 -0700

    add support for new lua format

--- src/file/lua.rs
+++ src/file/lua.rs
@@ -15,11 +15,21 @@ impl Extractor for LuaParser {
         shared_flex.clear();
 
         assert_eq!(1, variants.len());
-        for _ in 0..12 {
-            entry.read_u8().unwrap();
-        }
 
-        let header = entry.read_u32::<LE>().unwrap();
+        let body_size = variants[0].body_size;
+        let _ = entry.read_u32::<LE>();
+        let mut file_len = entry.read_u32::<LE>().unwrap();
+        let _ = entry.read_u32::<LE>();
+
+        let mut header = entry.read_u32::<LE>().unwrap();
+        if header == 2 {
+            let _ = entry.read_u32::<LE>();
+            let _ = entry.read_u32::<LE>();
+            header = entry.read_u32::<LE>().unwrap();
+        } else {
+            file_len = body_size;
+        }
+        let file_len = file_len as u64;
         assert!(header == 38423579 || header == 2186495515, "{:016x}.lua has unexpected header {header:08x}", entry.name);
 
         assert_eq!(entry.read_u8().unwrap(), 0);
@@ -42,7 +52,7 @@ impl Extractor for LuaParser {
         }
         let lua_path = std::str::from_utf8(&slice).unwrap();
 
-        io::copy(&mut entry, &mut *shared_flex).unwrap();
+        io::copy(&mut entry.take(file_len - shared_flex.len() as u64), &mut *shared_flex).unwrap();
 
         options.write(lua_path.as_ref(), &shared_flex)
     }
